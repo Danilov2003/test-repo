4a7eb5c7c2b12426fa472b1f7a57d5a32111ecef
Алина <91140022+caxarok19@users.noreply.github.com>
2021-11-23 14:52:02 +0300

    Add files via upload

--- jet/scripts/len.py
+++ jet/scripts/len.py
@@ -2,8 +2,8 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-y = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]
-x = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+y = [0, 0.1, 0.2, 0.5, 1, 2.5, 3, 3.5, 4, 4.5, 5]
+x = [0, 20, 40, 100, 200, 500, 600, 700, 800, 900, 1000]
 
 
 plt.errorbar(x, y, xerr=0, yerr=0, marker='.', ls="")
@@ -16,8 +16,8 @@ y_ = np.polyval(k, x)
 plt.plot(x, y_, 'c-')
 plt.tick_params(axis='both', which='major', labelsize=6)
 plt.title("График калибровки шага мотора", fontsize = 14)
-plt.xlabel("Смещения трубки Пито, см",  fontsize = 8) # ось абсцисс
-plt.ylabel("Количество шагов мотора",  fontsize = 8) # ось ординат
+plt.ylabel("Смещения трубки Пито, см",  fontsize = 8) # ось абсцисс
+plt.xlabel("Количество шагов мотора",  fontsize = 8) # ось ординат
 plt.grid()
 plt.savefig('график_1')
 plt.show()
